a073e1edc94a3a2d1ceeefd2d01b6fb9af553a93
Timothy Walters <waltim263@gmail.com>
2024-06-19 18:58:53 -0400

    completed problem

--- Daily_Coding_Problems/Daily coding problem 1282.py
+++ Daily_Coding_Problems/Daily coding problem 1282.py
@@ -7,8 +7,15 @@ Given a positive integer n, return the n-th perfect number.
 
 For example, given 1, you should return 19. Given 2, you should return 28.
 """
-
+total = 0
 n = [int(x) for x in input()]
-print(n)
-#test
 
+for number in n:
+    total += number
+if total > 10:
+    print("This number is not perfect, please choose a different number")
+elif total == 10:
+    print("This number is already perfect")
+else:
+    n.append(10-total)
+    print("Your perfect number is " + str(int(''.join(map(str, n)))))
