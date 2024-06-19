86cb4de82e6a74dcd0db9c4d1f97acfc24de7169
Timothy Walters <137981128+waltim263@users.noreply.github.com>
2024-06-19 18:20:34 -0400

    Add files via upload

---
+++ Learning/List Comprehensions.py
@@ -0,0 +1,11 @@
+"""
+https://www.youtube.com/watch?v=SKrrxKgkKG8
+"""
+
+#build a new list from existing list
+my_list = [1, 2, 3, 4, 5]
+
+new_list = [value * value for value in my_list]
+#you define the work to be done before the for loop
+print(new_list)
+
